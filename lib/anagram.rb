4856a359ca1c0b128c65539724532ec8d17127fe
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-31 00:01:42 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,14 @@
 # Your code goes here!
 class Anagram
+  attr_accessor :emails
 
-  
+  def initialize(emails)
+    @emails = emails
+  end
+
+  def parse
+    parsed_emails = emails.split(/[\s,]/)
+    parsed_emails.delete("")
+    parsed_emails.uniq
+  end  
 end
