2c11b9f7bf383b4965db3e7286038b277deab4e3
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-31 00:33:47 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,8 +9,8 @@ class Anagram
   def match(match_words)
     result = []
     word_arr = @words.split(//).sort
-    match_words.each do |words|
-      if words.split(//).sort == word_arr
+    match_words.each do |word|
+      if word.split(//).sort == word_arr
         result << words
       else
         result
